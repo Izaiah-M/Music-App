28abf8396237071c33fbbd700cdc98e8ddb9f30b
iz.aia.h <imukisa024@gmail.com>
2023-01-11 19:13:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Katuyiige</p>
+        <p>{props.like}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
