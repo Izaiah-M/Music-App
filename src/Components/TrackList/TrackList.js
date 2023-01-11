fd6a1fc8aea16911dfd07cbc849b2224d58b95be
iz.aia.h <imukisa024@gmail.com>
2023-01-11 23:13:50 +0300

    Added needed components for the app and styled them

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./TrackList.css";
+
+export class TrackList extends React.Component {
+  render() {
+    return (
+      <div className="TrackList">
+        {/* <!-- You will add a map method that renders a set of Track components  --> */}
+        <p>Bless Me by Maverick City</p>
+        <p>Hands In it by Mike</p>
+        <p>Joy by Tauren Wells</p>
+      </div>
+    );
+  }
+}
